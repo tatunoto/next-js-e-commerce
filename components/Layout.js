66d9b17b24f85013e8dc24b38e26ea84969214ce
tatunoto <79051857+tatunoto@users.noreply.github.com>
2021-03-05 18:28:43 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -13,8 +13,7 @@ function Layout({ children }) {
           <div className="flex items-center justify-between">
             <div className="hidden w-full text-gray-600 md:flex md:items-center">
              <img src="data:image/svg+xml;charset=utf8"
-              <svg
-              xmlns='http://www.w3.org/2000/svg'
+              <svg xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 64 64'
               aria-labelledby='title'
               aria-describedby='desc'
