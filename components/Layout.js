4f5694e9b47cd18361cb8c7c93ffa60573a991cd
tatunoto <79051857+tatunoto@users.noreply.github.com>
2021-03-05 03:40:08 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -126,9 +126,7 @@ function Layout({ children }) {
       <main className="my-8">{children}</main>
        <footer className="bg-gray-100 text-md font-medium text-gray-500">
         <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-         <div className="mt-3 sm:flex sm:justify-center sm:items-center">
-          Powered by <a href="https://www.delirium.design" className="hover:text-purple-400">Delirium Design</a> - 2021, all rights reserved
-         </div>
+         <span className="mt-3 sm:flex sm:justify-center sm:items-center text-md text-gray-700">Powered by <a href="https://www.delirium.design" className="hover:text-purple-400">Delirium Design</a> - 2021, all rights reserved</span>
         </div>
       </footer>
     </div>
