25a774a46c87ae7531d9418178ac9e1451df5f78
tatunoto <79051857+tatunoto@users.noreply.github.com>
2021-03-05 15:37:09 +0100

    Update Layout.js
    NameSpace

--- components/Layout.js
+++ components/Layout.js
@@ -12,7 +12,12 @@ function Layout({ children }) {
         <div className="container mx-auto px-6 py-3">
           <div className="flex items-center justify-between">
             <div className="hidden w-full text-gray-600 md:flex md:items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" aria-labelledby="title" aria-describedby="desc" role="img" xmlns:xlink="http://www.w3.org/1999/xlink"
+              <svg xmlns="http://www.w3.org/2000/svg"
+               viewBox="0 0 64 64"
+               aria-labelledby="title"
+               aria-describedby="desc"
+               role="img"
+               xmlns:xlink="http://www.w3.org/1999/xlink"
               >
                 <path
                   fillRule="evenodd"
