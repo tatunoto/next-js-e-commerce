e400ae59d4b8847e92e72bfbb9d354d73d406344
tatunoto <79051857+tatunoto@users.noreply.github.com>
2021-03-05 02:08:08 +0100

    Update Layout.js
    Delirium Design

--- components/Layout.js
+++ components/Layout.js
@@ -123,15 +123,15 @@ function Layout({ children }) {
       <Cart cartOpen={cartOpen} handleOpen={handleOpen} />
       }
       <main className="my-8">{children}</main>
-      <footer className="bg-gray-200">
+      <footer className="bg-gray-200 text-lg font-normal text-gray-500">
         <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-          <a
-            href="#"
-            className="text-xl font-bold text-gray-500 hover:text-gray-400"
+          Powered by <a
+            href="https://www.delirium.design"
+            className="hover:text-purple-400"
           >
-            Pulp Inc.
+            Delirium Design
           </a>
-          <p className="py-2 text-gray-500 sm:py-0">All rights reserved</p>
+          <p className="py-2 text-gray-500 sm:py-0">All rights reserved @</p>
         </div>
       </footer>
     </div>
