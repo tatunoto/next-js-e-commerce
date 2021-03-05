2d8c4781c30fcf1e1c66be738652907ad6acbdcc
tatunoto <79051857+tatunoto@users.noreply.github.com>
2021-03-05 18:51:17 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -12,7 +12,7 @@ function Layout({ children }) {
         <div className="container mx-auto px-6 py-3">
           <div className="flex items-center justify-between">
             <div className="hidden w-full text-gray-600 md:flex md:items-center">
-             <img src="data:image/svg+xml;charset=utf8">
+             <img src='data:image/svg+xml;charset=utf8,
               <svg xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 64 64'
               aria-labelledby='title'
@@ -39,7 +39,7 @@ function Layout({ children }) {
               stroke-width='2'
               stroke-linejoin='round'>
               </path>
-              </svg>
+              </svg>'
               <span className="mx-1 text-sm">060 555 333</span>
             </div>
             <div className="w-full text-gray-700 md:text-center text-2xl font-semibold">
