611f72d01443ea36ce5758625903f3c50a9b1bb8
tatunoto <79051857+tatunoto@users.noreply.github.com>
2021-03-06 01:06:28 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -13,8 +13,8 @@ function Layout({ children }) {
           <div className="flex items-center justify-between">
             <div className="hidden w-full text-gray-600 md:flex md:items-center">
              <svg 
-               className="h-5 w-5"
-               viewBox='0 0 64 64'
+               className="h-5 w-5 text-gray-500"
+               viewBox='0 0 24 24'
                fill="none"
                xmlns='http://www.w3.org/2000/svg'
                role='img'
@@ -22,14 +22,14 @@ function Layout({ children }) {
               <path
               d='M30.287 2.029A29.769 29.769 0 0 0 5.223 45.266L2.064 60.6a1.158 1.158 0 0 0 1.4 1.361L18.492 58.4A29.76 29.76 0 1 0 30.287 2.029zm17.931 46.2'
               stroke="currentColor"
-                  strokeWidth="4"
+                  strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
               />
               <path
               d='M46.184 38.205l-5.765-1.655a2.149 2.149 0 0 0-2.126.561l-1.41 1.436a2.1 2.1 0 0 1-2.283.482c-2.727-1.1-8.463-6.2-9.927-8.754a2.1 2.1 0 0 1 .166-2.328l1.23-1.592a2.148 2.148 0 0 0 .265-2.183l-2.424-5.485a2.149 2.149 0 0 0-3.356-.769c-1.609 1.361-3.517 3.428-3.749 5.719-.409 4.039 1.323 9.13 7.872 15.242 7.566 7.063 13.626 8 17.571 7.04 2.238-.542 4.026-2.714 5.154-4.493a2.15 2.15 0 0 0-1.218-3.221z'
               stroke="currentColor"
-                  strokeWidth="4"
+                  strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
               />
